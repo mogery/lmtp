7165b5e2cc8144f93dbd1f3442c55fd38b3fb245
Móricz <mo.geryy@gmail.com>
2021-04-04 11:12:41 +0200

    [VM] Add instruction number comments

--- vm.js
+++ vm.js
@@ -6,23 +6,23 @@
 function vm(c, input) {
     let r, i;
     const s = [], f = [
-        x => r = x,
-        x => i = x - 1,
-        (x, y) => x && (i = y - 1),
-        () => null,
-        () => ({JSON, input, Math, Date}),
-        () => String.fromCharCode,
-        x => s[x],
-        (x, y) => x[y],
-        (x, y, z) => (x[y] = z, x),
-        (x, y) => x.call(...s.splice(-y)),
-        (x, y) => x + y,
-        (x, y) => x * y,
-        x => -x,
-        x => !x,
-        (x, y) => x > y,
-        (x, y) => x == y,
-        x => typeof x
+        x => r = x, // 0
+        x => i = x - 1, // 1
+        (x, y) => x && (i = y - 1), // 2
+        () => null, // 3
+        () => ({JSON, input, Math, Date}), // 4
+        () => String.fromCharCode, // 5
+        x => s[x], // 6
+        (x, y) => x[y], // 7
+        (x, y, z) => (x[y] = z, x), // 8
+        (x, y) => x.call(...s.splice(-y)), // 9
+        (x, y) => x + y, // 10
+        (x, y) => x * y, // 11
+        x => -x, // 12
+        x => !x, // 13
+        (x, y) => x > y, // 14
+        (x, y) => x == y, // 15
+        x => typeof x // 16
     ];
 
     for (i = 0; !r; i++) {
